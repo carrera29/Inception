9c2e36749a24d4bc1427eadfb68494e3f561cc2c
Claudia Carrera <pollo@Claudias-MacBook-Pro.local>
2025-07-20 17:58:45 +0200

    para probar allí

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+set -e
+
 read LOGIN DOMAIN < <(head -n 1 /run/secrets/domain.txt | tr ':' ' ')
 
-# crear fichero de configuracion de nginx
+# create the nginx configuration file
 cat <<EOF > /etc/nginx/nginx.conf
 events {}
 http {
     include /etc/nginx/mime.types;
-
     server {
         listen 443 ssl;
         root /var/www/html;
         server_name ${LOGIN}.42.fr;
         index index.php;
-
         ssl_certificate /etc/nginx/ssl/inception.crt;
         ssl_certificate_key /etc/nginx/ssl/inception.key;
         ssl_protocols TLSv1.3;
-
         location ~ \.php$ {
             include snippets/fastcgi-php.conf;
             fastcgi_pass wordpress:9000;
@@ -26,9 +25,14 @@ http {
 }
 EOF
 
-# crear directorio de certificados y generar certificado autofirmado
+# make sure the directory for the SSL certificates exists
 mkdir -p /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/inception.key \
-    -out /etc/nginx/ssl/inception.crt \
-    -subj "/C=MO/ST=KH/O=42/OU=42/CN=${LOGIN}.42.fr"
+if [ ! -f /etc/nginx/ssl/inception.crt ] || [ ! -f /etc/nginx/ssl/inception.key ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/inception.key \
+        -out /etc/nginx/ssl/inception.crt \
+        -subj "/C=MO/ST=KH/O=42/OU=42/CN=${LOGIN}.42.fr"
+fi
+
+# execute nginx in the foreground
+exec nginx -g 'daemon off;'
