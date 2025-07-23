afa051f6bd11d6f3f9a61f17b2e23c17f6977fcd
Claudia Carrera <pollo@Claudias-MacBook-Pro.local>
2025-07-23 13:54:43 +0200

    ooOOk

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -2,30 +2,42 @@
 
 set -e
 
-read LOGIN DOMAIN < <(head -n 1 /run/secrets/domain.txt | tr ':' ' ')
-
 # create the nginx configuration file
-cat <<EOF > /etc/nginx/nginx.conf
+cat > /etc/nginx/nginx.conf << 'EOF'
 events {}
 http {
     include /etc/nginx/mime.types;
+
     server {
         listen 443 ssl;
+        listen [::]:443 ssl;
+
         root /var/www/html;
-        server_name ${LOGIN}.42.fr;
+        server_name clcarrer.42.fr ;
+
         index index.php;
+
         ssl_certificate /etc/nginx/ssl/inception.crt;
         ssl_certificate_key /etc/nginx/ssl/inception.key;
         ssl_protocols TLSv1.3;
+        
+        location / {
+            try_files $uri $uri/ /index.php$is_args$args;
+        }
+
         location ~ \.php$ {
-            include snippets/fastcgi-php.conf;
-            fastcgi_pass wordpress:9000;
+            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            fastcgi_pass wordpress:9000; #PHP for wordpress will listen on the port 9000
+            fastcgi_index index.php;
+            include fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            fastcgi_param SCRIPT_NAME $fastcgi_script_name;
         }
     }
 }
 EOF
 
-# make sure the directory for the SSL certificates exists
+# make sure the directory for the SSL certificates exists if not create it
 mkdir -p /etc/nginx/ssl
 if [ ! -f /etc/nginx/ssl/inception.crt ] || [ ! -f /etc/nginx/ssl/inception.key ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
